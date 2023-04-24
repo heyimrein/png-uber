7504a01f9fe56bc1a283d08d373898867f1b940c
reinlmao <rekreingd@gmail.com>
2023-04-23 22:32:15 -0500

    Add ui toggle
    'H' by default

--- src/main.rs
+++ src/main.rs
@@ -1,24 +1,31 @@
 mod egui_windows;
 
 use egui_macroquad::egui;
+use egui_macroquad::egui::{pos2};
 use macroquad::prelude::*;
 
 
 pub struct MainState {
-    path: String
+    show_ui: bool,
+    debug_path: String,
 }
 
 impl MainState {
     pub fn new() -> MainState {
         MainState {
-            path: "".to_string()
+            show_ui: true,
+            debug_path: "".to_string()
         }
     }
 }
 
+pub struct Tuber {
+
+}
+
 fn window_conf() -> Conf {
     Conf {
-        window_title: "PNG UBER".to_string(),
+        window_title: "PNG uber".to_string(),
         window_resizable: true,
         ..Default::default()
     }
@@ -32,15 +39,33 @@ async fn main() {
     loop {
         clear_background(BLACK);
 
-        egui_macroquad::ui(|ctx| {
-            egui::Window::new("egui test :>").show(
-                ctx,
-                |ui| { egui_windows::test_window(ui, &mut state) }
-            );
-        });
-        egui_macroquad::draw();
+        // Handle input
+        if is_key_pressed(KeyCode::H) {
+            state.show_ui = !state.show_ui;
+        }
 
-        draw_text(state.path.as_str(), 50., 50., 12., WHITE);
+        // Draw main graphics
+        draw_text(
+            state.debug_path.as_str(),
+            200.,
+            50.,
+            12.,
+            WHITE
+        );
+
+        // Handle and draw UI
+        if state.show_ui {
+            egui_macroquad::ui(|ctx| {
+                egui::Window::new("egui test :>")
+                    .fixed_pos(pos2(0., 0.))
+                    .fixed_size(egui::vec2(150., 100.))
+                    .show(
+                        ctx,
+                        |ui| { egui_windows::test_window(ui, &mut state) }
+                    );
+            });
+            egui_macroquad::draw();
+        }
 
         next_frame().await;
     }
