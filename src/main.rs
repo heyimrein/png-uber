4c779f160995f4c0bd0f6adc20b62bc6769284b0
reinlmao <rekreingd@gmail.com>
2023-04-23 21:40:10 -0500

    Add File Opening test

--- src/main.rs
+++ src/main.rs
@@ -3,6 +3,18 @@ mod egui_windows;
 use egui_macroquad::egui;
 use macroquad::prelude::*;
 
+pub struct MainState {
+    path: String
+}
+
+impl MainState {
+    pub fn new() -> MainState {
+        MainState {
+            path: "".to_string()
+        }
+    }
+}
+
 fn window_conf() -> Conf {
     Conf {
         window_title: "PNG UBER".to_string(),
@@ -13,13 +25,17 @@ fn window_conf() -> Conf {
 
 #[macroquad::main(window_conf)]
 async fn main() {
+    let mut state = MainState::new();
+
     loop {
         clear_background(BLACK);
+
+        draw_text(state.path.as_str(), 50., 50., 12., WHITE);
 
         egui_macroquad::ui(|ctx| {
             egui::Window::new("egui test :>").show(
                 ctx,
-                egui_windows::test_window
+                |ui| { egui_windows::test_window(ui, &mut state) }
             );
         });
         egui_macroquad::draw();
