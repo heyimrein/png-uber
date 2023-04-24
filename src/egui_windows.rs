2d348c90f41bf88689098440c2319ae3798bd841
reinlmao <rekreingd@gmail.com>
2023-04-23 21:39:55 -0500

    Add 'Open File' button to default ui window

--- src/egui_windows.rs
+++ src/egui_windows.rs
@@ -1,5 +1,24 @@
 use egui_macroquad::egui;
+use crate::MainState;
 
-pub fn test_window(ui: &mut egui::Ui) {
-    ui.label("test");
+pub fn test_window(ui: &mut egui::Ui, state: &mut MainState) {
+    if ui.button("Open File").clicked() {
+        let wrk_dir_option = std::env::current_dir().unwrap();
+        let wrk_dir = match wrk_dir_option.to_str() {
+            None => { return; }
+            Some(path) => { path }
+        };
+
+        let path = match tinyfiledialogs::open_file_dialog(
+            "Open File",
+            wrk_dir,
+            None,
+        ) {
+            None => { return; }
+            Some(path) => { path }
+        };
+
+        state.path = path;
+    }
 }
+
