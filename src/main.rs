d969476ccc8da1a87275eadb7ed8577880b45267
reinlmao <rekreingd@gmail.com>
2023-04-23 21:50:45 -0500

    Format code

--- src/main.rs
+++ src/main.rs
@@ -3,6 +3,7 @@ mod egui_windows;
 use egui_macroquad::egui;
 use macroquad::prelude::*;
 
+
 pub struct MainState {
     path: String
 }
@@ -23,14 +24,13 @@ fn window_conf() -> Conf {
     }
 }
 
+
 #[macroquad::main(window_conf)]
 async fn main() {
     let mut state = MainState::new();
 
     loop {
         clear_background(BLACK);
-
-        draw_text(state.path.as_str(), 50., 50., 12., WHITE);
 
         egui_macroquad::ui(|ctx| {
             egui::Window::new("egui test :>").show(
@@ -39,6 +39,8 @@ async fn main() {
             );
         });
         egui_macroquad::draw();
+
+        draw_text(state.path.as_str(), 50., 50., 12., WHITE);
 
         next_frame().await;
     }
